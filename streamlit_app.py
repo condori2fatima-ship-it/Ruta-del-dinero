22f74ea2688c91c77f7964551e092b27724be23b
condori2fatima-ship-it <condori2fatima@gmail.com>
2026-06-09 21:31:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,14 +272,14 @@ elif ingreso_mensual <= 0:
 
 else:
 
-dinero_disponible = ingreso_mensual - gastos_mensuales
-ahorro_necesario = monto_objetivo / plazo_meses
-ahorro_segun_porcentaje = ingreso_mensual * (porcentaje_ahorro_elegido / 100)
+    dinero_disponible = ingreso_mensual - gastos_mensuales
+    ahorro_necesario = monto_objetivo / plazo_meses
+    ahorro_segun_porcentaje = ingreso_mensual * (porcentaje_ahorro_elegido / 100)
 
-porcentaje_ahorro_disponible = (dinero_disponible / ingreso_mensual) * 100
-porcentaje_necesario = (ahorro_necesario / ingreso_mensual) * 100
+    porcentaje_ahorro_disponible = (dinero_disponible / ingreso_mensual) * 100
+    porcentaje_necesario = (ahorro_necesario / ingreso_mensual) * 100
 
-meses_estimados = monto_objetivo / ahorro_segun_porcentaje
+    meses_estimados = monto_objetivo / ahorro_segun_porcentaje
 
     
 # ---------------- RESULTADOS ----------------
@@ -289,12 +289,12 @@ st.markdown(
         unsafe_allow_html=True
     )
     
-col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
     
-col1.metric("Monto objetivo", formato_moneda(monto_objetivo, simbolo))
-col2.metric("Ahorro necesario", formato_moneda(ahorro_necesario, simbolo))
-col3.metric("Disponible mensual", formato_moneda(dinero_disponible, simbolo))
-col4.metric("Ahorro elegido", formato_moneda(ahorro_segun_porcentaje, simbolo))
+    col1.metric("Monto objetivo", formato_moneda(monto_objetivo, simbolo))
+    col2.metric("Ahorro necesario", formato_moneda(ahorro_necesario, simbolo))
+    col3.metric("Disponible mensual", formato_moneda(dinero_disponible, simbolo))
+    col4.metric("Ahorro elegido", formato_moneda(ahorro_segun_porcentaje, simbolo))
 
 st.write("**Objetivo:**", objetivo)
 st.write("**Porcentaje necesario para cumplir el objetivo:**", round(porcentaje_necesario, 2), "%")
